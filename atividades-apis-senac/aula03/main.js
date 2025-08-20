199015c8d6a024ffb1537426e2a4927448fd5056
lorenzopandolfo <lorenzopandolfo2004@gmail.com>
2025-08-19 22:03:52 -0300

    aula3

--- atividades-apis-senac/aula03/main.js
+++ atividades-apis-senac/aula03/main.js
@@ -2,20 +2,23 @@ import { Product } from "./domain/user.js";
 import { Categories } from "./enum/categories.js";
 import { ProductRepository } from "./repository/product_repository.js";
 
-const repository = new ProductRepository([coca, sprite]);
-
 const coca = new Product("Coca cola", Categories.DRINK, 5.5);
 const sprite = new Product("Sprite", Categories.DRINK, 2.5);
 const fanta = new Product("Fanta Uva", Categories.DRINK, 3.0);
 
-// separar por metodos
+const repository = new ProductRepository([coca, sprite]);
 
+// separar por metodos
 const fetchCoca = repository.findById(coca.id);
 
 let fetchAll = repository.findAll();
 
-console.log(fetchCoca);
+console.log("Coca já existente no banco:", fetchCoca);
 
 repository.save(fanta);
 
-console.log(fetchAll);
+const fetchFanta = repository.findById(fanta.id);
+
+console.log("Fanta adicionada no banco:", fetchFanta);
+
+console.log("Banco de dados atual: ", fetchAll);
